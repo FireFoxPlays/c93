882985d9b61e5a2840a1c4a4df5be77e4d6e004c
FireFoxPlays <88514805+FireFoxPlays@users.noreply.github.com>
2022-07-27 15:01:29 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,23 @@
+user_name = localStorage.getItem("user_name")
+document.getElementById("welcome").innerHTML = "Welcome " + user_name;
+
+
+
+
+
+
+
+
+
+
+
+
+//ADD YOUR FIREBASE LINKS HERE
+
+function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+
+      //End code
+      });});}
+getData();
